145d50d1f21851af7d81a657dc12cc30b5f405b0
Aina Dylan Rasamoelina <hei.dylan.3@gmail.com>
2023-12-08 12:59:55 +0300

    feat: updated database structure and added table CurrencyValues

--- script.sql
+++ script.sql
@@ -4,41 +4,101 @@ CREATE DATABASE wallet_management;
 
 \c wallet_management
 
--- Création de la table Currency s'il n'existe pas
+-- Création de la table Currency
 CREATE TABLE IF NOT EXISTS Currency (
-    currency_id SERIAL PRIMARY KEY,
-    currency_name VARCHAR(255) NOT NULL,
-    exchange_rate DOUBLE PRECISION NOT NULL
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(255),
+    code VARCHAR(10)
 );
 
--- Insertion de données factices dans la table Currency
-INSERT INTO Currency (currency_name, exchange_rate) VALUES
-    ('EUROS', 3000.10),
-    ('ARIARY', 1.0),
-    ('DOLLAR', 2000.12);
+-- Inserts fictifs pour Currency
+INSERT INTO Currency (id, name, code) VALUES
+    ('USD', 'US Dollar', 'USD'),
+    ('EUR', 'Euro', 'EUR'),
+    ('JPY', 'Japanese Yen', 'JPY');
 
--- Création de la table Account s'il n'existe pas
-CREATE TABLE IF NOT EXISTS Account (
-    account_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL,
-    balance DOUBLE PRECISION NOT NULL,
-    currency_id INTEGER REFERENCES Currency(currency_id) ON DELETE CASCADE
+-- Création de la table TransactionType
+CREATE TABLE IF NOT EXISTS TransactionType (
+    id SERIAL PRIMARY KEY,
+    type_name VARCHAR(50) UNIQUE
 );
 
--- Insertion de données factices dans la table Account
-INSERT INTO Account (user_name, balance, currency_id) VALUES
-    ('Tiavina Andriatsitohery', 500.00, 1),
-    ('Aina Dylan', 700.00, 1);
+-- Inserts fictifs pour TransactionType
+-- (Nous n'insérons rien ici, car ces données sont généralement fixes et définies dans le script de création)
 
--- Création de la table Transaction s'il n'existe pas
+-- Création de la table Transaction
 CREATE TABLE IF NOT EXISTS Transaction (
-    transaction_id SERIAL PRIMARY KEY,
-    amount DOUBLE PRECISION NOT NULL,
-    account_source_id INTEGER REFERENCES Account(account_id) ON DELETE CASCADE,
-    account_destination_id INTEGER REFERENCES Account(account_id) ON DELETE CASCADE
+    id VARCHAR(50) PRIMARY KEY,
+    label VARCHAR(255),
+    amount DOUBLE PRECISION,
+    date TIMESTAMP,
+    type_id INT REFERENCES TransactionType(id)
 );
 
--- Insertion de données factices dans la table Transaction
-INSERT INTO Transaction (amount, account_source_id, account_destination_id) VALUES
-    (1500.00, 1, 2),
-    (5000.00, 2, 1);
+-- Inserts fictifs pour Transaction
+INSERT INTO Transaction (id, label, amount, date, type_id) VALUES
+    ('1', 'Purchase', 100.50, '2023-12-08 12:00:00', 1),
+    ('2', 'Salary', 2000.00, '2023-12-09 09:30:00', 2);
+
+-- Création de la table AccountType
+CREATE TABLE IF NOT EXISTS AccountType (
+    id SERIAL PRIMARY KEY,
+    type_name VARCHAR(50) UNIQUE
+);
+
+-- Inserts fictifs pour AccountType
+INSERT INTO AccountType (type_name) VALUES
+    ('BANK'),
+    ('CASH'),
+    ('MOBILE_MONEY');
+
+-- Création de la table Account
+CREATE TABLE IF NOT EXISTS Account (
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(255),
+    balance DOUBLE PRECISION,
+    last_update TIMESTAMP,
+    currency_id VARCHAR(50) REFERENCES Currency(id),
+    type_id INT REFERENCES AccountType(id)
+);
+
+-- Inserts fictifs pour Account
+INSERT INTO Account (id, name, balance, last_update, currency_id, type_id) VALUES
+    ('A123', 'Main Bank Account', 1500.75, '2023-12-08 15:45:00', 'USD', 1),
+    ('A456', 'Wallet', 50.20, '2023-12-09 10:15:00', 'EUR', 2);
+
+-- Création de la table TransactionAccount (relation many-to-many entre Account et Transaction)
+CREATE TABLE IF NOT EXISTS TransactionAccount (
+    account_id VARCHAR(50) REFERENCES Account(id),
+    transaction_id VARCHAR(50) REFERENCES Transaction(id),
+    PRIMARY KEY (account_id, transaction_id)
+);
+
+-- Inserts pour TransactionAccount (relation many-to-many)
+INSERT INTO TransactionAccount (account_id, transaction_id) VALUES
+    ('A123', '1'),
+    ('A456', '2');
+
+-- Création de la table CurrencyValue
+CREATE TABLE IF NOT EXISTS CurrencyValue (
+    id SERIAL PRIMARY KEY,
+    base_currency_id VARCHAR(50) REFERENCES Currency(id),
+    target_currency_id VARCHAR(50) REFERENCES Currency(id),
+    value DOUBLE PRECISION,
+    value_date TIMESTAMP
+);
+
+-- Inserts fictifs pour CurrencyValue
+INSERT INTO CurrencyValue (base_currency_id, target_currency_id, value, value_date) VALUES
+    ('USD', 'EUR', 0.85, '2023-12-08 16:00:00'),
+    ('USD', 'JPY', 115.20, '2023-12-08 16:00:00'),
+    ('EUR', 'USD', 1.18, '2023-12-08 16:00:00'),
+    ('EUR', 'JPY', 135.50, '2023-12-08 16:00:00'),
+    ('JPY', 'USD', 0.0087, '2023-12-08 16:00:00'),
+    ('JPY', 'EUR', 0.0074, '2023-12-08 16:00:00');
+
+
+
+
+
+
